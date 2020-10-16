cfc0e0cbaa6ff2709555211b93b4f5cbf79b3e36
ByoengsuPark <39683194+ByeongsuPark@users.noreply.github.com>
2020-10-16 19:54:35 +0900

    [GH-42]Add autocomplete icon (#46)

--- server/command.go
+++ server/command.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/mattermost/mattermost-plugin-api/experimental/command"
 	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mattermost-server/v5/plugin"
+	"github.com/pkg/errors"
 )
 
 const helpText = `
@@ -41,15 +43,22 @@ const helpText = `
 const jobNotSpecifiedResponse = "Please specify a job name to build."
 const pollingSleepTime = 10
 
-func getCommand() *model.Command {
-	return &model.Command{
-		Trigger:          "jenkins",
-		Description:      "A Mattermost plugin to interact with Jenkins",
-		DisplayName:      "Jenkins",
-		AutoComplete:     true,
-		AutoCompleteDesc: "Available commands: connect, disconnect, me, build, get-artifacts, test-results, get-log, abort, disable, enable, delete, safe-restart, plugins, createjob, help",
-		AutoCompleteHint: "[command]",
+func (p *Plugin) getCommand() (*model.Command, error) {
+	iconData, err := command.GetIconData(p.API, "assets/icon.svg")
+
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get icon data")
 	}
+
+	return &model.Command{
+		Trigger:              "jenkins",
+		Description:          "A Mattermost plugin to interact with Jenkins",
+		DisplayName:          "Jenkins",
+		AutoComplete:         true,
+		AutoCompleteDesc:     "Available commands: connect, disconnect, me, build, get-artifacts, test-results, get-log, abort, disable, enable, delete, safe-restart, plugins, createjob, help",
+		AutoCompleteHint:     "[command]",
+		AutocompleteIconData: iconData,
+	}, nil
 }
 
 func (p *Plugin) postCommandResponse(args *model.CommandArgs, text string) {
